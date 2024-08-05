7c44b03cd14406f1798f6776bc220d3d40243cdd
sanathsb98 <itssanathsb@gmail.com>
2024-08-05 14:03:52 +0530

    commit

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Test route to check if server is running
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Server is up and running!');
 });
 
